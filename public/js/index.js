ebe3ccd246aa48715c5c6447ce479d6c4563f0e6
Alex Macleod <alex@macleod.io>
2016-05-18 16:26:42 +0100

    Added animated timers

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,13 @@ import PouchDB from 'pouchdb'
 PouchDB.debug.enable('*')
 
 const db = new PouchDB('test-001')
+let timerRunning = false
 
 db.changes({
   since: 'now',
   live: true
 }).on('change', change => {
-  console.log(change)
+  console.log('change: ', change)
   updateUI()
 })
 
@@ -17,7 +18,8 @@ db.get('001').then(doc => {
   return db.put({
     _id: '001',
     _rev: doc._rev,
-    text: `goodbye ${doc._rev}`
+    text: `supersedes ${doc._rev}`,
+    period: 30000
   })
 }).catch(err => {
   if (err.status !== 404) throw err
@@ -27,12 +29,88 @@ db.get('001').then(doc => {
   })
 })
 
-function updateUI () {
-  const entries = document.getElementById('entries')
+db.get('002').then(doc => {
+  console.log(doc)
+  return db.put({
+    _id: '002',
+    _rev: doc._rev,
+    text: `supersedes ${doc._rev}`,
+    period: 60000
+  })
+}).catch(err => {
+  if (err.status !== 404) throw err
+  db.put({
+    _id: '002',
+    text: 'hello'
+  })
+})
 
+function updateUI () {
+  timerRunning = false
   db.allDocs({
     include_docs: true
   }).then(result => {
-    console.log(result)
+    const entries = document.getElementById('entries')
+
+    while (entries.firstChild) entries.removeChild(entries.firstChild)
+
+    for (let row of result.rows) {
+      const entry = document.createElement('div')
+
+      const entryText = document.createElement('div')
+      entryText.classList.add('entryText')
+      entryText.textContent = row.doc.text
+
+      const entryTimer = document.createElement('canvas')
+      entryTimer.classList.add('entryTimer')
+      entryTimer.setAttribute('data-period', row.doc.period)
+      entryTimer.setAttribute('width', 50)
+      entryTimer.setAttribute('height', 50)
+
+      entry.appendChild(entryText)
+      entry.appendChild(entryTimer)
+      entries.appendChild(entry)
+    }
+
+    timerRunning = true
+    window.requestAnimationFrame(timer)
   })
+}
+
+function timer () {
+  if (!timerRunning) return
+
+  const entries = document.getElementById('entries')
+
+  for (let entry of entries.children) {
+    const entryTimer = entry.querySelector('.entryTimer')
+    // entryTimer.textContent = time % entryTimer.getAttribute('data-period')
+    drawTimer(entryTimer)
+  }
+
+  window.requestAnimationFrame(timer)
+}
+
+function drawTimer (canvas) {
+  if (!canvas.getContext) return
+  const radius = canvas.width / 2
+  const period = canvas.getAttribute('data-period')
+  const time = Date.now() % period
+  const endAngle = 1.5 * Math.PI + 2 * time * Math.PI / period
+
+  let ctx = canvas.getContext('2d')
+
+  ctx.clearRect(0, 0, canvas.width, canvas.width)
+  ctx.strokeStyle = '#42A5F5'
+  ctx.fillStyle = '#42A5F5'
+
+  ctx.beginPath()
+  ctx.arc(radius, radius, radius, 0, 2 * Math.PI)
+  ctx.stroke()
+
+  ctx.beginPath()
+  ctx.arc(radius, radius, radius, Math.PI * 1.5, endAngle, true)
+  ctx.lineTo(radius, radius)
+  ctx.lineTo(radius, 0)
+  ctx.fill()
 }
